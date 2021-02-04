ad4ae678d36bf8cfff75172247a96f1fc4570eba
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-02-04 15:16:22 +0200

    listasivulle e2e testejä

--- client/cypress/integration/recycling-app.spec.js
+++ client/cypress/integration/recycling-app.spec.js
@@ -1,6 +1,22 @@
 describe('Recycling app', function() {
-  it('front page can be opened', function() {
+  it('productlist page can be opened', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Kotitalouden kierrätysavustin')
+  })
+  it('productlist contains products', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Mustamakkarakastike pullo')
+    cy.contains('Maitotölkki')
+    cy.contains('Sanomalehti')
+  })
+  it('product recycling information can be opened by clicking button', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Mustamakkarakastike pullo')
+    const stub = cy.stub()
+    cy.on ('window:alert', stub)
+    cy.get('button').contains('Näytä kierrätysohje').click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Mustamakkarakastike pullo - Irrota korkki, huuhtele pullo. Laita pullo ja korkki muovinkeräykseen erillään toisistaan.')
+      })
   })
 })
