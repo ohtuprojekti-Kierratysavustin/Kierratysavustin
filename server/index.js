0978455d6b73cf81f620561baf3bd15795f44da9
nicholsss <mickyaw1995@gmail.com>
2021-02-11 13:05:36 +0200

    tietokannan alustus alkuun

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const Product = require('./models/product')
+const Instruction = require('./models/instruction')
+const user = require('./models/user')
+const comment = require('./models/comment')
+
