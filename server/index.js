d4181f97b211338d0fe95acd4d7e8e73d1debc64
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-02-14 15:48:12 +0200

    Alustavan testidatan lisäys backendin puolelle

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
+const products = [
+    {
+      id: 1,
+      name: 'Mustamakkarakastike pullo',
+      instructions: 'Irrota korkki, huuhtele pullo. Laita pullo ja korkki muovinkeräykseen erillään toisistaan.'
+    },
+    {
+      id: 2,
+      name: 'Maitotölkki',
+      instructions: 'Huuhtele tölkki. Laita litistettynä kartonkikeräykseen.'
+    },
+    {
+      id: 3,
+      name: 'Sanomalehti',
+      instructions: 'Laita lehti paperinkeräykseen.'
+    }
+  ]
+
 
 const Product = require('./models/product')
 const Instruction = require('./models/instruction')
 const User = require('./models/user')
 const Comment = require('./models/comment')
-
 
 const express = require('express')
 const app = express()
@@ -17,7 +34,22 @@ app.use(express.static('build'))
 
 const PORT = 3001
 
+  
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const product = products.find(product => product.id === id)
+    response.json(product)
+  })
+
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
-
