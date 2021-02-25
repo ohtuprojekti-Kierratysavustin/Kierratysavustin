17a2a25ff099d2f8fab6f229292b11acd3af1f52
nicholsss <mickyaw1995@gmail.com>
2021-02-25 13:36:11 +0200

    user can be created

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
-
+const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type:String, required:true}
+    username: {type: String, required: true,unique:true},
+    passwordHash: String
 })
-
+userSchema.plugin(uniqueValidator)
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 module.exports = mongoose.model("User", userSchema)
