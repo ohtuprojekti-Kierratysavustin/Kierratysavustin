8e3b4cb92af579a00cd6668fc4ba28a62b775f37
hiski <hiski.r@gmail.com>
2021-02-26 16:48:50 +0200

    action test

--- e2e_tests/cypress/integration/recycling-app.spec.js
+++ e2e_tests/cypress/integration/recycling-app.spec.js
@@ -1,24 +1,22 @@
 describe('Recycling app', function() {
   it('productlist page can be opened', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.contains('Kotitalouden kierrätysavustin')
   })
   it('Product can be added to application', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.get('#productForm').click()
     cy.get('#nameInput').type('Muovipussi')
     cy.get('#descriptionInput').type('Muovia')
     cy.get('#addproductBtn').click()
   })
   it('productlist contains products', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.reload()
-    cy.contains('Mustamakkarakastike pullo')
-    cy.contains('Maitotölkki')
     cy.contains('Muovipussi')
   })
   it('product can be clicked and instruction can be added', function(){
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.contains('Muovipussi').click()
     cy.get('#instructionInput').type('Tuotteen voi uudelleen käyttää roskapussina')
     cy.get('#addInstruction').click()
