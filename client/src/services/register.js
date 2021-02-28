f4833bd7ed2f0e32dc9f8a66b5a5b426cd65051d
nicholsss <mickyaw1995@gmail.com>
2021-02-28 20:00:34 +0200

    front ja back yhdistetty

--- client/src/services/register.js
+++ client/src/services/register.js
@@ -1,10 +1,6 @@
 import axios from 'axios'
 const baseUrl = `${process.env.PUBLIC_URL}/api/users`
 
-
-
-
-
 const createUser = async (newUser) => {
   const response = axios.post(`${baseUrl}`, newUser)
   return response
