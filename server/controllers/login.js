afe3c9c43504236b2680249a7d208866cd8ffcb7
nicholsss <mickyaw1995@gmail.com>
2021-03-19 11:22:03 +0200

    m

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,32 +1,33 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
-const { response } = require('express')
-loginRouter.post('/', async (req, res) => {
-    
-    try {
-        
-        
-        const body = req.body
-        
-        const user = await User.findOne({ username: body.username })
-        
-        console.log(user)
-        const correctPswrd = user === null ? false : await bcrypt.compare(body.password, user.passwordHash)
-        
-        if (!(user && correctPswrd)) { 
-           
-            return res.status(401).json({ error: "Väärä nimi tai salasana"}) 
-        } 
-        require('dotenv').config(); 
-        const token = jwt.sign(user.username, process.env.SECRET)
-        
-        res.status(200).json({ token, username: user.username})
-    
-    }catch (error) {
-        
-        return res.status(401).json({ error: "Väärä nimi tai salasana" })
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const loginRouter = require("express").Router()
+const User = require("../models/user")
+const { response } = require("express")
+require("dotenv").config()
+loginRouter.post("/", async (req, res) => {
+  
+    const body = req.body
+    const user = await User.findOne({ username: body.username })
+    console.log(user)
+    const correctPswrd =
+      user === null
+        ? false
+        : await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && correctPswrd)) {
+        console.log('vaarin if')
+      return res.status(401).json({ error: "Väärä nimi tai salasana" })
     }
+    const userToken = {
+        username: user.username,
+        id: user.id
+    }
+    const token = jwt.sign(userToken,process.env.SECRET)
+
+    res.status(200).json({ token, username: user.username })
+
+      console.log('lol')
+    return res.status(401).json({ error: "Väärä nimi tai salasana" })
+
 })
 module.exports = loginRouter
