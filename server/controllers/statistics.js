d9cbcafe13dc8461152283a94a070f3f1647bf7f
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-10-11 16:47:46 +0300

    Use populate instead of filter map find creation

--- server/controllers/statistics.js
+++ server/controllers/statistics.js
@@ -1,5 +1,5 @@
 const statisticsRouter = require('express').Router()
-const Product = require('../models/product')
+// const Product = require('../models/product')
 const ProductUserCounter = require('../models/productUserCounter')
 const authUtils = require('../utils/auth')
 const STATUS_CODES = require('http-status')
@@ -8,17 +8,8 @@ const { restructureCastAndValidationErrorsFromMongoose } = require('../error/exc
 statisticsRouter.get('/', async (req, res, next) => {
   try {
     let user = await authUtils.authenticateRequestReturnUser(req)
-    const allProducts = await Product.find({})
-    const productStats = await ProductUserCounter.find({userID: user.id})
-    const filteredProducts = allProducts.filter(product => productStats.some(stat =>
-      product._id.toString() === stat.productID.toString()
-    )).map(product => {
-      const matchingStatistic = productStats.find(stat =>
-        product._id.toString() === stat.productID.toString()
-      )
-      return {...product.toJSON(), recycleCount: matchingStatistic.recycleCount, purchaseCount: matchingStatistic.purchaseCount}
-    })
-    res.status(STATUS_CODES.OK).json(filteredProducts)
+    const productStats = await ProductUserCounter.find({userID: user.id}).populate('productID')
+    res.status(STATUS_CODES.OK).json(productStats)
   } catch (error) {
     let handledError = restructureCastAndValidationErrorsFromMongoose(error)
     next(handledError)
