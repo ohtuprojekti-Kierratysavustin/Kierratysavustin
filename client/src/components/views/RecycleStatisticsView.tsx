7295f9c472ed9184acc10926ca8e046ad541b1bd
Iira <iira.hakkinen@helsinki.fi>
2021-10-25 13:28:43 +0300

    päivämäärät x-akselille

--- client/src/components/views/RecycleStatisticsView.tsx
+++ client/src/components/views/RecycleStatisticsView.tsx
@@ -44,8 +44,9 @@ const RecycleStatisticsView = () => {
     }
   ))
 
+  // kuvaajan datan tyyppi
   type dataValues = {
-    labels: number[],
+    labels: string[],
     datasets: [
       {
         label: string,
@@ -56,26 +57,19 @@ const RecycleStatisticsView = () => {
     ]
   }
 
-  //kuvaajan testaamiseen, koska ei vielä bäkkäriä mistä oikea data
-  /*
-  const chartDataForTesting: dataValues = {
-    labels: [1,2,3,4,5,6,7],
-    datasets: [
-      {
-        label: 'Päivittäinen kierrätysaste',
-        data: [0.3,0.3,0.5,0.6,0.6,0.87,0.6],
-        fill: false,
-        borderColor: '#137447'
-      }
-    ]
+  // päivämäärät x-akselille
+  const today: Date = new Date()
+  const dates: string[] = []
+  for (let i = 30; i >= 0; i--) {
+    const date: Date = new Date()
+    date.setDate(today.getDate() - i)
+    console.log(date.getDay())
+    dates.push(`${date.getDate()}.${date.getMonth() + 1}.`)
   }
-  */
 
-  //data kuvaajaan
-
-
+  // data kuvaajaan
   const chartData: dataValues = {
-    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    labels: dates,
     datasets: [
       {
         label: 'Päivittäinen kierrätysaste',
