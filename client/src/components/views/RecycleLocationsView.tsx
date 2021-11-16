5c4bc0fcd45fce2feccedf1f15865149e0566da9
Iira <iira.hakkinen@helsinki.fi>
2021-11-16 13:15:35 +0200

    paikkakunnalla ja postinumerolla haku samaan kenttään

--- client/src/components/views/RecycleLocationsView.tsx
+++ client/src/components/views/RecycleLocationsView.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import InfoBar from '../InfoBar'
 import Map from '../Map'
-//import { RecyclingSpot } from '../../types/objects'
+import { useStore } from '../../store'
 import kierratysInfoService from '../../services/kierratysInfo'
 import { Container, Button, Form, Row, Col } from 'react-bootstrap'
+import { ErrorResponse } from '../../types/requestResponses'
 
 // käyttäjän paikan haku:
 //navigator.geolocation.getCurrentPosition((position) => {
@@ -13,33 +14,59 @@ import { Container, Button, Form, Row, Col } from 'react-bootstrap'
 
 const RecycleLocationsView = () => {
   var defaultCoordinates: [number, number] = [60.150, 24.96]
-  const [municipality, setMunicipality] = useState('')
-  const [postalCode, setPostalCode] = useState('')
+  const { setNotification } = useStore()
+  const [input, setInput] = useState('')
   const [recyclingSpots, setRecyclingSpots] = useState([])
   const [mapCenter, setMapCenter] = useState(defaultCoordinates)
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault()
     var coordinates: [number, number] = defaultCoordinates
+    var isPostalCode = /[0-9]{5}/
 
-    if (municipality !== '') {
-      await kierratysInfoService.getCollectionSpotsByMunicipality(municipality)
+    if (isPostalCode.test(input)) {    // jos hakusanana postinumero
+      await kierratysInfoService.getCollectionSpotsByPostalCode(input)
+        .then(result => {
+          setRecyclingSpots(result.results)
+          console.log(result.results)
+          coordinates = [result.results[0].geometry.coordinates[1], result.results[0].geometry.coordinates[0]]
+        })
+        .catch((error: ErrorResponse) => {
+          setNotification((`Postinumerolla ${input} haettaessa tapahtui virhe!`)
+            , 'error')
+          console.log(error.message)
+        })
+
+    } else {  // hakusanana paikkakunta tai joku muu
+      await kierratysInfoService.getCollectionSpotsByMunicipality(input)
         .then(result => {
           setRecyclingSpots(result.results)
           coordinates = [result.results[0].geometry.coordinates[1], result.results[0].geometry.coordinates[0]]
         })
-    }
-    if (postalCode !== '') {
-      await kierratysInfoService.getCollectionSpotsByPostalCode(postalCode)
-        .then(result => {
-          setRecyclingSpots(result.results)
-          coordinates = [result.results[0].geometry.coordinates[1], result.results[0].geometry.coordinates[0]]
+        .catch((error: ErrorResponse) => {
+          setNotification((`Hakusanalla ${input} haettaessa tapahtui virhe!`)
+            , 'error')
+          console.log(error.message)
         })
     }
-    setMunicipality('')
-    setPostalCode('')
+
+    setInput('')
     setMapCenter(coordinates)
   }
+
+  /*
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    var input = event.target.value
+
+    // hakusanana postinumero
+    if (Number.isInteger(input) && input.length===5) {
+      setPostalCode(input)
+      setMunicipality('')
+    } else {  //hakusanana paikkakunta (tai mitä tahansa muuta)
+      setMunicipality(input)
+    }
+  }
+  */
 
   return (
     <div>
@@ -48,22 +75,15 @@ const RecycleLocationsView = () => {
         <Container>
           <Form id='kierratysinfoHaku' onSubmit={onSubmit}>
             <Form.Group as={Row}>
-              <Col md="auto">
+              <Col md="6">
                 <Form.Control
                   type="text"
-                  placeholder="Kirjoita paikkakunnan nimi..."
-                  id="paikkakuntaInput"
-                  onChange={({ target }) => setMunicipality(target.value)} />
-              </Col>
-              <Col md='auto'>
-                <Form.Control
-                  type='text'
-                  placeholder='Tai kirjoita postinumero...'
-                  id='postinumeroInput'
-                  onChange={({ target }) => setPostalCode(target.value)} />
+                  placeholder="Kirjoita paikkakunnan nimi tai postinumero..."
+                  id="hakusanaInput"
+                  onChange={({ target }) => setInput(target.value)} />
               </Col>
               <Col>
-                <Button id='paikkakuntaSubmit' type="submit">
+                <Button id='hakusanaSubmit' type="submit">
                   Hae
                 </Button>
               </Col>
