27023b3fcc5930fb61943ed52a123b8e1241d59d
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-12-14 15:28:13 +0200

    Resolve conflicts

--- client/src/components/ProductsList.tsx
+++ client/src/components/ProductsList.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import {
-  Link
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import FavoritesForm from './forms/FavoritesForm'
 import logo from '../media/logo.png'
 import { Container, Media, ListGroup, Row, Col } from 'react-bootstrap'
@@ -26,17 +24,26 @@ const ProductsList: React.FC<Props> = ({ products, header }) => {
       <Container>
         <h2>{header}</h2>
         <ListGroup as='ul' id='list'>
-          {products.map(product =>
-            <ListGroup.Item action as='li' key={product.id} id='list-item'>
-              <Link style={{ textDecoration: 'none' }} to={`/products/${product.id}`}>
-                <Media>
-                  <img
-                    className="product-image-list-view"
-                    src={product.productImage ? `${process.env.PUBLIC_URL}/api/files/images/${product.productImage}` : logo}
-                    alt=""
-                  />
-                  {/* Toinen tapa kuvan croppaamiseen keskitetysti tietyyn kokoon */}
-                  {/* <div
+          {products.length === 0
+            ? <Container>
+              <Row>
+                <Col>
+                  <h4>Ei tuotteita!</h4>
+                </Col>
+              </Row>
+            </Container>
+            : products.map(product =>
+              <ListGroup.Item action as='li' key={product.id} id='list-item'>
+                <Link style={{ textDecoration: 'none' }} to={`/products/${product.id}`}>
+
+                  <Media>
+                    <img
+                      className="product-image-list-view"
+                      src={product.productImage ? `${process.env.PUBLIC_URL}/api/files/images/${product.productImage}` : logo}
+                      alt=""
+                    />
+                    {/* Toinen tapa kuvan croppaamiseen keskitetysti tietyyn kokoon */}
+                    {/* <div
                     style={{
                       width: '200px',
                       height: '200px',
@@ -45,78 +52,79 @@ const ProductsList: React.FC<Props> = ({ products, header }) => {
                       backgroundSize: 'cover'
                     }}
                   /> */}
-                  <Media.Body>
-                    <Container>
-                      <Row>
-                        <Col sm={8}>
-                          <Row>
-                            <h5>{product.name}</h5>
-                          </Row>
-                          <Row>
-                            {product.instructions.length === 0 ? '' : product.instructions[0].information.length >= 50 ? (
-                              <p>
-                                Suositelluin ohje: {product.instructions[0].information.slice(0, 50)}...
-                              </p>
-                            ) : (
-                              <p>
-                                Suositelluin ohje: {product.instructions[0].information}
-                              </p>
-                            )}
-                          </Row>
-                          <Row>
-                            {user !== null ? (
-                              <div className='ListItemButtons'>
-                                <Col>
-                                  <FavoritesForm product={product} />
-                                </Col>
-                                <Col>
-                                  <DeleteProduct product={product} />
-                                </Col>
-                              </div>
-                            ) : (
-                              ''
-                            )}
-                          </Row>
-                        </Col>
-                        {user !== null ?
-                          <>
-                            <Col sm={2}>
-                              <ProductUserCountForm
-                                product={product}
-                                countType={PRODUCT_USER_COUNT_REQUEST_TYPE.PURCHASE}
-                                amountText={'Hankittu'}
-                                sendUpdateText={'Hanki'}
-                                subtractUpdateText={'Vähennä'}
-                                tooltipAdd={'Kasvata tuotteen hankintatilastoa.'}
-                                tooltipDelete={'Vähennä tuotteen hankintatilastoa.'}
-                                counterService={counterService}
-                                statisticsService={undefined}
-                                setChartData={undefined}
-                              />
-                            </Col>
-                            <Col sm={2}>
-                              <ProductUserCountForm
-                                product={product}
-                                countType={PRODUCT_USER_COUNT_REQUEST_TYPE.RECYCLE}
-                                amountText={'Kierrätetty'}
-                                sendUpdateText={'Kierrätä'}
-                                subtractUpdateText={'Vähennä'}
-                                tooltipAdd={'Kasvata tuotteen kierrätystilastoa.'}
-                                tooltipDelete={'Vähennä tuotteen kierrätystilastoa.'}
-                                counterService={counterService}
-                                statisticsService={undefined}
-                                setChartData={undefined}
-                              />
-                            </Col>
-                          </>
-                          : ''}
-                      </Row>
-                    </Container>
-                  </Media.Body>
-                </Media>
-              </Link>
-            </ListGroup.Item>
-          )}
+                    <Media.Body>
+                      <Container>
+                        <Row>
+                          <Col sm={8}>
+                            <Row>
+                              <h5>{product.name}</h5>
+                            </Row>
+                            <Row>
+                              {product.instructions.length === 0 ? '' : product.instructions[0].information.length >= 50 ? (
+                                <p>
+                                  Suositelluin ohje: {product.instructions[0].information.slice(0, 50)}...
+                                </p>
+                              ) : (
+                                <p>
+                                  Suositelluin ohje: {product.instructions[0].information.slice(0, 50)}
+                                </p>
+                              )}
+                            </Row>
+                            <Row>
+                              {user !== null ? (
+                                <div className='ListItemButtons'>
+                                  <Col>
+                                    <FavoritesForm product={product} />
+                                  </Col>
+                                  <Col>
+                                    <DeleteProduct product={product} />
+                                  </Col>
+                                </div>
+                              ) : (
+                                ''
+                              )}
+                            </Row>
+                          </Col>
+                          {user !== null ?
+                            <>
+                              <Col sm={2}>
+                                <ProductUserCountForm
+                                  product={product}
+                                  countType={PRODUCT_USER_COUNT_REQUEST_TYPE.PURCHASE}
+                                  amountText={'Hankittu'}
+                                  sendUpdateText={'Hanki'}
+                                  subtractUpdateText={'Vähennä'}
+                                  tooltipAdd={'Kasvata tuotteen hankintatilastoa.'}
+                                  tooltipDelete={'Vähennä tuotteen hankintatilastoa.'}
+                                  counterService={counterService}
+                                  statisticsService={undefined}
+                                  setChartData={undefined}
+                                />
+                              </Col>
+                              <Col sm={2}>
+                                <ProductUserCountForm
+                                  product={product}
+                                  countType={PRODUCT_USER_COUNT_REQUEST_TYPE.RECYCLE}
+                                  amountText={'Kierrätetty'}
+                                  sendUpdateText={'Kierrätä'}
+                                  subtractUpdateText={'Vähennä'}
+                                  tooltipAdd={'Kasvata tuotteen kierrätystilastoa.'}
+                                  tooltipDelete={'Vähennä tuotteen kierrätystilastoa.'}
+                                  counterService={counterService}
+                                  statisticsService={undefined}
+                                  setChartData={undefined}
+                                />
+                              </Col>
+                            </>
+                            : ''}
+                        </Row>
+                      </Container>
+                    </Media.Body>
+                  </Media>
+                </Link>
+
+              </ListGroup.Item>
+            )}
         </ListGroup>
       </Container>
     </div>
